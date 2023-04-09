7423c5b39bdff5fdf26b1cc48d0b6b4a0434b6d2
lgow <@>
2023-04-09 16:00:45 -0400

    Initial contibuition (cracked blocks; optimized block registration; parchment implementation; creative tab/build.gradle/mods.toml/gradle.properties organization

--- src/main/java/us/blueeyemods/blueeyecommons/Main.java
+++ src/main/java/us/blueeyemods/blueeyecommons/Main.java
@@ -1,14 +1,7 @@
 package us.blueeyemods.blueeyecommons;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.CreativeModeTabEvent;
@@ -17,11 +10,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 import us.blueeyemods.blueeyecommons.block.ModBlocks;
 import us.blueeyemods.blueeyecommons.item.ModCreativeModeTabs;
@@ -35,17 +24,10 @@ public class Main {
 
     public Main() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
         ModItems.register(modEventBus);
-
         ModBlocks.register(modEventBus);
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
     }
 
@@ -53,69 +35,38 @@ public class Main {
     }
 
     private void addCreative(CreativeModeTabEvent.BuildContents event) {
-        if(event.getTab() == CreativeModeTabs.BUILDING_BLOCKS) {
+        if(event.getTab() == CreativeModeTabs.BUILDING_BLOCKS || event.getTab() == ModCreativeModeTabs.TAB) {
             event.accept(ModBlocks.PACKED_SNOW);
-            event.accept(ModBlocks.PACKED_SNOW_SLAB);
             event.accept(ModBlocks.PACKED_SNOW_STAIRS);
+            event.accept(ModBlocks.PACKED_SNOW_SLAB);
             event.accept(ModBlocks.PACKED_SNOW_WALL);
+            event.accept(ModBlocks.CHISELED_PACKED_SNOW);
             event.accept(ModBlocks.PACKED_SNOW_BRICKS);
             event.accept(ModBlocks.PACKED_SNOW_BRICK_STAIRS);
             event.accept(ModBlocks.PACKED_SNOW_TILES);
             event.accept(ModBlocks.PACKED_SNOW_TILE_STAIRS);
             event.accept(ModBlocks.PACKED_SNOW_PILLAR);
-            event.accept(ModBlocks.CHISELED_PACKED_SNOW);
-            event.accept(ModBlocks.PACKED_ICE_SLAB);
             event.accept(ModBlocks.PACKED_ICE_STAIRS);
+            event.accept(ModBlocks.PACKED_ICE_SLAB);
             event.accept(ModBlocks.PACKED_ICE_WALL);
+            event.accept(ModBlocks.CHISELED_PACKED_ICE);
             event.accept(ModBlocks.PACKED_ICE_BRICKS);
-            event.accept(ModBlocks.PACKED_ICE_TILES);
             event.accept(ModBlocks.CRACKED_PACKED_ICE_BRICKS);
+            event.accept(ModBlocks.PACKED_ICE_TILES);
             event.accept(ModBlocks.CRACKED_PACKED_ICE_TILES);
             event.accept(ModBlocks.PACKED_ICE_PILLAR);
-            event.accept(ModBlocks.CHISELED_PACKED_ICE);
-            event.accept(ModBlocks.BLUE_ICE_SLAB);
             event.accept(ModBlocks.BLUE_ICE_STAIRS);
+            event.accept(ModBlocks.BLUE_ICE_SLAB);
             event.accept(ModBlocks.BLUE_ICE_WALL);
+            event.accept(ModBlocks.CHISELED_BLUE_ICE);
             event.accept(ModBlocks.BLUE_ICE_BRICKS);
-            event.accept(ModBlocks.BLUE_ICE_TILES);
             event.accept(ModBlocks.CRACKED_BLUE_ICE_BRICKS);
+            event.accept(ModBlocks.BLUE_ICE_TILES);
             event.accept(ModBlocks.CRACKED_BLUE_ICE_TILES);
             event.accept(ModBlocks.BLUE_ICE_PILLAR);
-            event.accept(ModBlocks.CHISELED_BLUE_ICE);
-        }
-        if(event.getTab() == ModCreativeModeTabs.TAB1) {
-            event.accept(ModBlocks.PACKED_SNOW);
-            event.accept(ModBlocks.PACKED_SNOW_SLAB);
-            event.accept(ModBlocks.PACKED_SNOW_STAIRS);
-            event.accept(ModBlocks.PACKED_SNOW_WALL);
-            event.accept(ModBlocks.PACKED_SNOW_BRICKS);
-            event.accept(ModBlocks.PACKED_SNOW_BRICK_STAIRS);
-            event.accept(ModBlocks.PACKED_SNOW_TILES);
-            event.accept(ModBlocks.PACKED_SNOW_TILE_STAIRS);
-            event.accept(ModBlocks.PACKED_SNOW_PILLAR);
-            event.accept(ModBlocks.CHISELED_PACKED_SNOW);
-            event.accept(ModBlocks.PACKED_ICE_SLAB);
-            event.accept(ModBlocks.PACKED_ICE_STAIRS);
-            event.accept(ModBlocks.PACKED_ICE_WALL);
-            event.accept(ModBlocks.PACKED_ICE_BRICKS);
-            event.accept(ModBlocks.PACKED_ICE_TILES);
-            event.accept(ModBlocks.CRACKED_PACKED_ICE_BRICKS);
-            event.accept(ModBlocks.CRACKED_PACKED_ICE_TILES);
-            event.accept(ModBlocks.PACKED_ICE_PILLAR);
-            event.accept(ModBlocks.CHISELED_PACKED_ICE);
-            event.accept(ModBlocks.BLUE_ICE_SLAB);
-            event.accept(ModBlocks.BLUE_ICE_STAIRS);
-            event.accept(ModBlocks.BLUE_ICE_WALL);
-            event.accept(ModBlocks.BLUE_ICE_BRICKS);
-            event.accept(ModBlocks.BLUE_ICE_TILES);
-            event.accept(ModBlocks.CRACKED_BLUE_ICE_BRICKS);
-            event.accept(ModBlocks.CRACKED_BLUE_ICE_TILES);
-            event.accept(ModBlocks.BLUE_ICE_PILLAR);
-            event.accept(ModBlocks.CHISELED_BLUE_ICE);
         }
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
 
